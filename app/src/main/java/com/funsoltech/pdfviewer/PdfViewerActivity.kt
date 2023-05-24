8c42000a1c0e8cdb9a26fd3172072f2f46776b42
Sajid Shah <sajidshah.funsol@gmail.com>
2023-05-24 18:17:18 +0500

    -fetch pdf files in App

--- app/src/main/java/com/funsoltech/pdfviewer/PdfViewerActivity.kt
+++ app/src/main/java/com/funsoltech/pdfviewer/PdfViewerActivity.kt
@@ -1,14 +1,21 @@
 package com.funsoltech.pdfviewer
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.documentfile.provider.DocumentFile
+import java.io.File
+import java.io.FilenameFilter
+import java.util.Arrays
+
 
 class PdfViewerActivity : AppCompatActivity() {
     private val viewPdfLauncher =
@@ -36,8 +43,22 @@ class PdfViewerActivity : AppCompatActivity() {
         viewPdfButton = findViewById(R.id.btn_view_pdf)
         viewPdfButton.setOnClickListener {
             browsePdfFiles()
+            val pdfFiles: MutableList<File> = getPdfFiles()
+
+// Display the list of PDF files to the user.
+
+// Display the list of PDF files to the user.
+            for (file in pdfFiles) {
+                Log.d("PDF_VIEWER", file.name)
+            }
         }
+
     }
+    private fun getPdfFiles(): MutableList<File> {
+
+    }
+
+
 
     private fun browsePdfFiles() {
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
@@ -47,6 +68,7 @@ class PdfViewerActivity : AppCompatActivity() {
         viewPdfLauncher.launch(intent)
     }
 
+    @SuppressLint("QueryPermissionsNeeded")
     private fun viewPdfFile(uri: Uri) {
         val intent = Intent(Intent.ACTION_VIEW).apply {
             setDataAndType(uri, "application/pdf")
