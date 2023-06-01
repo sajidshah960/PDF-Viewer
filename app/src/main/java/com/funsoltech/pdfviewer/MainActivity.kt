6abdb239a12320dd962e97542d69bd84c74840e4
Sajid Shah <sajidshah.funsol@gmail.com>
2023-06-01 17:29:04 +0500

    - bottom navigation menu added

--- app/src/main/java/com/funsoltech/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/funsoltech/pdfviewer/MainActivity.kt
@@ -5,8 +5,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle,
+    ) {
+        super.onRestoreInstanceState(savedInstanceState)
+}
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +42,37 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, CaptureImageActivity::class.java)
             startActivity(intent)
         }
-    }
 
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_dashboard -> {
+                    val intent = Intent(this, SetAlarmActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.navigation_home -> {
+                    val intent = Intent(this, PdfViewerActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.navigation_notifications -> {
+                    val intent = Intent(this, CaptureImageActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                else -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    false
+                }
+
+            }
+        }
+    }
 }
 
 
