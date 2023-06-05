a7e4526c05f258926e6d954a3a81beaba982c013
Sajid Shah <sajidshah.funsol@gmail.com>
2023-06-05 18:19:50 +0500

    - in progress

--- app/src/main/java/com/funsoltech/pdfviewer/MainActivityTabView.kt
+++ app/src/main/java/com/funsoltech/pdfviewer/MainActivityTabView.kt
@@ -2,14 +2,20 @@ package com.funsoltech.pdfviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivityTabView : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_tab_view)
@@ -22,6 +28,33 @@ class MainActivityTabView : AppCompatActivity() {
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = "Tab ${position + 1}"
         }.attach()
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        // Handle settings button click
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_settings -> {
+                    // show setting menu
+                    loadSettingsFragment()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+    private fun loadSettingsFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, SettingsFragment(), "SettingsFragment")
+            .commit()
     }
 }
 
