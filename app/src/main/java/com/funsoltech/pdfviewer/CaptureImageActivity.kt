7a53f12a1b837f860c690da86ef267837489b14d
Sajid Shah <sajidshah.funsol@gmail.com>
2023-06-07 18:13:41 +0500

    - add Glider with blur effect - changed few screens

--- app/src/main/java/com/funsoltech/pdfviewer/CaptureImageActivity.kt
+++ app/src/main/java/com/funsoltech/pdfviewer/CaptureImageActivity.kt
@@ -1,15 +1,27 @@
 package com.funsoltech.pdfviewer
 
+import android.R.attr.height
+import android.R.attr.width
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Switch
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.documentfile.provider.DocumentFile
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.FutureTarget
+import jp.wasabeef.glide.transformations.BlurTransformation
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class CaptureImageActivity : AppCompatActivity() {
     private val browserImageLauncher =
@@ -43,7 +55,7 @@ class CaptureImageActivity : AppCompatActivity() {
 
 
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "UseSwitchCompatOrMaterialCode", "CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_capture_image)
@@ -55,18 +67,43 @@ class CaptureImageActivity : AppCompatActivity() {
             browseImageFiles()
         }
 
-        var captureButton = findViewById<Button>(R.id.btn_capture_image)
-        captureButton.setOnClickListener {
-            captureImage()
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        var toggleButton = findViewById<Switch>(R.id.toggleButton)
+        var imageLink =
+            "https://moneymint.com/wp-content/uploads/2022/04/Best-URL-Shortener-to-Earn-Money-Online-1024x683.png"
+        var bitmap: Bitmap? = null
+        // load image in bitmap
+        GlobalScope.launch(Dispatchers.IO) {
+            // Load the bitmap on a background thread
+            val futureTarget: FutureTarget<Bitmap> = Glide.with(this@CaptureImageActivity)
+                .asBitmap()
+                .load(imageLink)
+                .timeout(10000)
+                .submit()
+
+            bitmap= futureTarget.get()
+
+            // Display the bitmap on the UI thread
+            withContext(Dispatchers.Main) {
+                imageView.setImageBitmap(bitmap)
+            }
         }
 
+
+
+        toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Glide.with(this)
+                    .load(bitmap)
+                    .transform(BlurTransformation(25, 3))
+                    .into(imageView)
+            } else {
+                // Remove blur effect
+                imageView.setImageBitmap(bitmap)
+            }
+        }
     }
 
-    private fun captureImage() {
-        // open camera app
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivity(intent)
-    }
 
     private fun browseImageFiles() {
         val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
@@ -76,5 +113,5 @@ class CaptureImageActivity : AppCompatActivity() {
         browserImageLauncher.launch(intent)
 
     }
-
 }
+
--- app/src/main/java/com/funsoltech/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/funsoltech/pdfviewer/MainActivity.kt
@@ -2,21 +2,36 @@ package com.funsoltech.pdfviewer
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.documentfile.provider.DocumentFile
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-    }
-    override fun onRestoreInstanceState(
-        savedInstanceState: Bundle,
-    ) {
-        super.onRestoreInstanceState(savedInstanceState)
-}
+
+    private val viewPdfLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                data?.data?.let { uri ->
+                    val documentFile = DocumentFile.fromSingleUri(this, uri)
+                    if (documentFile != null && documentFile.type?.startsWith("application/pdf") == true) {
+                        viewPdfFile(uri)
+                    } else {
+                        Toast.makeText(this, "Selected file is not a valid PDF file.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,34 +40,24 @@ class MainActivity : AppCompatActivity() {
         // create alarm button
         val actionAlarmButton = findViewById<Button>(R.id.btn_create_alarm)
         actionAlarmButton.setOnClickListener {
-            val intent = Intent(this, SetAlarmActivity::class.java)
-            startActivity(intent)
+            createAlarm("Alarm", 12, 0)
         }
 
         // view pdf button
         val actionViewPdfButton = findViewById<Button>(R.id.btn_view_pdfs)
         actionViewPdfButton.setOnClickListener {
-            val intent = Intent(this, PdfViewerActivity::class.java)
-            startActivity(intent)
+            browsePdfFiles()
         }
 
         // take photo button
         val actionTakePhotoButton = findViewById<Button>(R.id.btn_capture_image)
         actionTakePhotoButton.setOnClickListener {
-            val intent = Intent(this, CaptureImageActivity::class.java)
-            startActivity(intent)
+            captureImage()
         }
-
-
 
         bottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_dashboard -> {
-                    val intent = Intent(this, SetAlarmActivity::class.java)
-                    startActivity(intent)
-                    true
-                }
                 R.id.navigation_home -> {
                     val intent = Intent(this, PdfViewerActivity::class.java)
                     startActivity(intent)
@@ -76,6 +81,62 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1){
+            if (grantResults.isNotEmpty() && grantResults[0] == android.content.pm.PackageManager.PERMISSION_GRANTED){
+                createAlarm("Alarm", 12, 0)
+            }
+            else{
+                Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun browsePdfFiles() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/pdf"
+        }
+        viewPdfLauncher.launch(intent)
+    }
+    private fun viewPdfFile(uri: Uri) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(uri, "application/pdf")
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.e("PDF_VIEWER", "No PDF viewer app found.")
+        }
+    }
+
+    private fun captureImage() {
+        // open camera app
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivity(intent)
+    }
+
+    private fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+        else{
+            Toast.makeText(this, "No alarm app found.", Toast.LENGTH_SHORT).show()
         }
     }
 }
